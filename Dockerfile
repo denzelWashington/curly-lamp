a1bce2125381f9745402b1907fa8ac421e773b82
Wassim Sboui <wassim.sboui@gmail.com>
2026-03-22 08:31:33 +0100

    feat: add docker compose and enhance dockerfile

--- Dockerfile
+++ Dockerfile
@@ -7,8 +7,8 @@ RUN ["mvn", "package", "-DskipTests"]
 
 FROM eclipse-temurin:21-alpine AS executor
 WORKDIR /opt/app
-RUN adduser fdeadm
+RUN adduser -D fdeadm
 COPY --from=builder /opt/project/target/*.jar ./app.jar
 RUN chown fdeadm app.jar
-USER appuser
-ENTRYPOINT ["java", "-jar", "app.jar"]  
+USER fdeadm
+ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,14 +1,20 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Autowired
+    private Environment env;
+
     @GetMapping("/hello")
     public String hello() {
-        return "Hello MVC 1234";
+        String pod = env.getProperty("POD_NAME", "unknown");
+        return "Hello le nom du pod est " + pod;
     }
 
     @GetMapping("/wait")
